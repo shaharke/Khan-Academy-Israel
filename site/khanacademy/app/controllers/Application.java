ca6f8ded5b206ff678026532408d22e3f2a2ad5c
Shahar Kedar <shahar@gmail.com>
2011-10-19 11:40:57 +0200

    Switching to play framework

---
+++ site/khanacademy/app/controllers/Application.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+	
+    public static void home() {
+    	render();
+    }
+    
+    public static void news() {
+    	render();
+    }
+    
+    public static void forum() {
+    	render();
+    }
+    
+    public static void contribute() {
+    	render();
+    }
+    
+}
---
+++ site/khanacademy/app/controllers/Contact.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import play.data.validation.Required;
+import play.i18n.Messages;
+import play.libs.Mail;
+import play.mvc.Controller;
+
+public class Contact extends Controller {
+
+	public static void form() {
+		render();
+	}
+
+	public static void send(@Required(message="contact.name.required") String name, String email, @Required(message="contact.subject.required") String subject, @Required(message="contact.message.required") String message) throws EmailException {
+    	if (validation.hasErrors()) {
+    		validation.keep();
+    		params.flash();
+    		form();
+    	}
+    	SimpleEmail mail = new SimpleEmail();
+    	mail.setCharset("UTF-8");
+    	mail.setFrom("user.khan.ac.il@gmail.com", "Khan Academy Israel User");
+//    	mail.addTo("hebrewkhan@gmail.com");
+    	mail.addTo("shahar@gmail.com");
+    	mail.setSubject(subject);
+    	message = message + "\n\n" + "This message was sent by " + email;
+    	mail.setMsg(message);
+    	Mail.send(mail);
+    	render();
+    }
+}
