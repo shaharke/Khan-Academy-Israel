5a82862b8e7e2c20007066c5a372b0342f1c9a71
Shahar Kedar <shahar@gmail.com>
2011-10-29 12:31:38 +0200

    Deleting unnecessary folders

--- site/khanacademy/app/controllers/Application.java
+++ site/khanacademy/app/controllers/Application.java
@@ -1,28 +1,57 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.util.*;
-
-import models.*;
+import play.libs.OpenID;
+import play.libs.OpenID.UserInfo;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 	
-    public static void home() {
-    	render();
-    }
-    
-    public static void news() {
-    	render();
-    }
-    
-    public static void forum() {
-    	render();
-    }
-    
-    public static void contribute() {
-    	render();
-    }
-    
+	private static Map<String, String> endpointMapping = new HashMap<String, String>();
+	
+	static {
+		endpointMapping.put("google", "https://www.google.com/accounts/o8/id");
+	}
+
+	public static void home() {
+		render();
+	}
+
+	public static void news() {
+		render();
+	}
+
+	public static void forum() {
+		render();
+	}
+
+	public static void contribute() {
+		render();
+	}
+
+	public static void login() {
+		render();
+	}
+
+	public static void authenticate(String serviceId) {
+		String endpoint = endpointMapping.get(serviceId);
+		if (endpoint != null) {
+			if (OpenID.isAuthenticationResponse()) {
+				UserInfo verifiedUser = OpenID.getVerifiedID();
+				if (verifiedUser == null) {
+					flash.error("Oops. Authentication has failed");
+					login();
+				}
+				session.put("user", verifiedUser.id);
+			}
+			if (!OpenID.id(serviceId).verify()) {
+				flash.error("Cannot verify your OpenID");
+				login();
+			}
+			home();
+		} 
+	}
+
 }
