b243732730ca19fd0e7ff441297f3919f69e7ab3
Shahar Kedar <shahar@gmail.com>
2011-12-11 20:56:43 +0200

    Preparing site for first deployment

--- site/sql/insert_topics.sql
+++ site/sql/insert_topics.sql
@@ -1,1 +1,1 @@
-EDIT `khan`.`topic`;
+﻿INSERT INTO `khan`.`topic` (`id`, `description`, `englishName`, `name`) VALUES (1, 'שיעורים אלו מהווים הכנה לקראת שיעורי האלגברה', 'Pre-Algebra', 'קדם אלגברה');
