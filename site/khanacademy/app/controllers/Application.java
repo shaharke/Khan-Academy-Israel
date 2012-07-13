2a0794d37e5969456d2403e930c4d785bc6f463e
Shahar Kedar <shahar@gmail.com>
2012-07-13 13:15:55 +0300

    Catalog should be on a different page

--- site/khanacademy/app/controllers/Application.java
+++ site/khanacademy/app/controllers/Application.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.List;
 
+import models.Lesson;
+import models.SubTopic;
 import models.Topic;
 import play.mvc.Controller;
 
@@ -10,21 +12,30 @@ public class Application extends Controller {
 	public static void home() {
 		render();
 	}
-
-	public static void news() {
-		render();
+	
+	public static void catalog() {
+		List<Topic> topics = Topic.all().fetch();
+		sort(topics);
+		render(topics);
 	}
-
-	public static void forum() {
-		render();
-	}
-
+	
 	public static void contribute() {
 		render();
 	}
 
 	public static void login() {
 		render();
+	}
+	
+	private static void sort(List<Topic> topics) {
+		Topic.sort(topics);
+		for (Topic t : topics) {
+			List<SubTopic> subtopics = t.subtopics;
+			SubTopic.sort(subtopics);
+			for (SubTopic st : subtopics) {
+				Lesson.sort(st.lessons);
+			}
+		}
 	}
 	
 }
