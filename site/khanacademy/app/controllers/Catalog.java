ae01ca66298d43020766ba242f3845bcb4c9150a
Shahar Kedar <shahar@gmail.com>
2012-09-15 12:48:58 +0300

    Expose the catalog index on a different url for embedding purposes

--- site/khanacademy/app/controllers/Catalog.java
+++ site/khanacademy/app/controllers/Catalog.java
@@ -10,10 +10,18 @@ import play.mvc.Controller;
 public class Catalog extends Controller {
 
     public static void show() {
+    	List<Topic> topics = Topic.all().fetch();
+		sort(topics);
+    	render(topics);
+    }
+    
+    public static void index() {
 		List<Topic> topics = Topic.all().fetch();
 		sort(topics);
-    	renderTemplate("/Catalog/catalog.html", topics);
+    	render(topics);
     }
+    
+    
 
 	private static void sort(List<Topic> topics) {
 		Topic.sort(topics);
---
+++ site/khanacademy/public/javascripts/catalog.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+		$('.lessons').columnize({
+			columns : 3,
+			width : 300,
+			'float' : 'right',
+			lastNeverTallest : false
+		});
+	});
